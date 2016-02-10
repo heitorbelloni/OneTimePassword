be7fb72f0adf95ba5b2bdf278580dbcd5ea4184d
Heitor Belloni <heitor.belloni@gmail.com>
2016-02-10 16:43:23 -0200

    OneTimePasswordController: 'Validate' method + unit tests

--- OneTimePassword.Tests/Web/Controllers/OneTimePasswordControllerTests.cs
+++ OneTimePassword.Tests/Web/Controllers/OneTimePasswordControllerTests.cs
@@ -37,5 +37,43 @@ namespace OneTimePassword.Tests.Web.Controllers {
             var result = _controller.Generate("userId");
             Assert.AreEqual("token", result.Data);
         }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("   ")]
+        public void Validate_should_throw_http_exception_with_bad_request_status_code_when_user_id_is_null_or_white_space(string userId) {
+            var result = Assert.Throws<HttpException>(() => _controller.Validate(userId, It.IsAny<string>()));
+            Assert.AreEqual((int) HttpStatusCode.BadRequest, result.GetHttpCode());
+            Assert.AreEqual("\"User ID\" is required.", result.Message);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("   ")]
+        public void Validate_should_throw_http_exception_with_bad_request_status_code_when_token_is_null_or_white_space(string token) {
+            var result = Assert.Throws<HttpException>(() => _controller.Validate("userId", token));
+            Assert.AreEqual((int) HttpStatusCode.BadRequest, result.GetHttpCode());
+            Assert.AreEqual("\"Token\" is required.", result.Message);
+        }
+
+        [Test]
+        public void Validate_should_call_generator_to_get_a_token() {
+            _controller.Validate("userId", "token");
+            Mock.Get(_generator).Verify(g => g.Generate(It.Is<string>(userId => userId.Equals("userId"))), Times.Once);
+        }
+
+        [Test]
+        public void Validate_should_return_true_if_parameter_token_and_generated_token_are_equal() {
+            Mock.Get(_generator).Setup(g => g.Generate(It.IsAny<string>())).Returns("token");
+            var result = _controller.Validate("userId", "token");
+            Assert.IsTrue((bool) result.Data);
+        }
+
+        [Test]
+        public void Validate_should_return_false_if_parameter_token_and_generated_token_are_different() {
+            Mock.Get(_generator).Setup(g => g.Generate(It.IsAny<string>())).Returns("differentToken");
+            var result = _controller.Validate("userId", "token");
+            Assert.IsFalse((bool) result.Data);
+        }
     }
 }
--- OneTimePassword.Web/Controllers/OneTimePasswordController.cs
+++ OneTimePassword.Web/Controllers/OneTimePasswordController.cs
@@ -19,5 +19,17 @@ namespace OneTimePassword.Web.Controllers {
             var token = _oneTimePasswordGenerator.Generate(userId);
             return Json(token, JsonRequestBehavior.AllowGet);
         }
+
+        public JsonResult Validate(string userId, string token) {
+            if (String.IsNullOrWhiteSpace(userId)) {
+                throw new HttpException((int) HttpStatusCode.BadRequest, "\"User ID\" is required.");
+            }
+            if (String.IsNullOrWhiteSpace(token)) {
+                throw new HttpException((int) HttpStatusCode.BadRequest, "\"Token\" is required.");
+            }
+            var checkToken = _oneTimePasswordGenerator.Generate(userId);
+            var result = token.Equals(checkToken);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
     }
 }
