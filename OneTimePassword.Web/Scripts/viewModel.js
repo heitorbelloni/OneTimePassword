9c20b997a628c97cd9e984287bde5f93d7fb28db
Heitor Belloni <heitor.belloni@gmail.com>
2016-02-11 16:47:29 -0200

    viewModel refactoring + implementing validate (wip) + display error message

--- OneTimePassword.Web/Scripts/viewModel.js
+++ OneTimePassword.Web/Scripts/viewModel.js
@@ -5,30 +5,50 @@ var my = my || {};
 my.viewModel = (function () {
     var userId = ko.observable();
     var token = ko.observable();
+    var message = ko.observable();
 
     var generate = function () {
         var url = "/OneTimePassword/Generate";
-        $.getJSON(url, {
-            userId: userId()
-        }).done(function (data) {
-            token(data);
-        }).fail(function (xhr) {
-            var error = xhr.responseText;
-            Console.log(error);
-        });
+        var parameters = { userId: userId() };
+        var done = function (result) {
+            token(result);
+        };
+        getJson(url, parameters, done);
     };
 
     var validate = function () {
-
+        var url = "/OneTimePassword/Validate";
+        var parameters = {
+            userId: userId(),
+            token: token()
+        };
+        var done = function (result) {
+            //
+        };
+        getJson(url, parameters, done);
     };
 
     var getJson = function (url, parameters, done) {
+        clear();
 
+        $.getJSON(url, parameters)
+        .done(function (result) {
+            done(result);
+        })
+        .fail(function (xhr) {
+            var error = xhr.responseText;
+            message(error);
+        });
+    };
+
+    var clear = function() {
+        message("");
     };
 
     return {
         userId: userId,
         token: token,
+        message: message,
         generate: generate,
         validate: validate,
     }
