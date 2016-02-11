fa62d8905fa52fdb802fa3dca337b03d1f229691
Heitor Belloni <heitor.belloni@gmail.com>
2016-02-11 16:05:12 -0200

    add and use ko viewModel (wip)

---
+++ OneTimePassword.Web/Scripts/viewModel.js
@@ -0,0 +1,39 @@
+﻿"use strict";
+
+var my = my || {};
+
+my.viewModel = (function () {
+    var userId = ko.observable();
+    var token = ko.observable();
+
+    var generate = function () {
+        var url = "/OneTimePassword/Generate";
+        $.getJSON(url, {
+            userId: userId()
+        }).done(function (data) {
+            token(data);
+        }).fail(function (xhr) {
+            var error = xhr.responseText;
+            Console.log(error);
+        });
+    };
+
+    var validate = function () {
+
+    };
+
+    var getJson = function (url, parameters, done) {
+
+    };
+
+    return {
+        userId: userId,
+        token: token,
+        generate: generate,
+        validate: validate,
+    }
+}());
+
+$(document).ready(function () {
+    ko.applyBindings(my.viewModel);
+});
