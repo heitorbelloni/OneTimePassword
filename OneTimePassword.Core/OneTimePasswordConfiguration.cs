27d108ffe2248740a1627ab4d61ada32e2664542
Heitor Belloni <heitor.belloni@gmail.com>
2016-10-31 12:15:30 -0200

    default values for test/presentation purposes

--- OneTimePassword.Core/OneTimePasswordConfiguration.cs
+++ OneTimePassword.Core/OneTimePasswordConfiguration.cs
@@ -3,19 +3,19 @@
 namespace OneTimePassword.Core {
     public class OneTimePasswordConfiguration : IOneTimePasswordConfiguration {
         public string GetSecrectKey() {
-            throw new NotImplementedException();
+            return "5e3dc25b-9a7f-4b28-80a8-973f95ae17b1";
         }
 
         public DateTime GetEpoch() {
-            throw new NotImplementedException();
+            return DateTime.MinValue;
         }
 
         public int GetIntervalInSeconds() {
-            throw new NotImplementedException();
+            return 30;
         }
 
         public int GetTokenLenght() {
-            throw new NotImplementedException();
+            return 6;
         }
     }
 }
